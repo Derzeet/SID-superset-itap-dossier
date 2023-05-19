c1c90b807bd99bfabf903dbaccd83293c5cfed39
Grisha <cocarecu@gmail.com>
2023-05-19 14:39:35 +0600

    CALENDAR WORKERS

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-import allLocales from '@fullcalendar/core/locales-all';
+// import allLocales from '@fullcalendar/core/locales-all';
 import './Calendar.css'
 
 function CalendarPage(props) {
